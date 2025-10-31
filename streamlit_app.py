71c2b7cc1968f5290b878b864df3210dc578e74d
FirdainiAzmi <firdaini.23071@mhs.unesa.ac.id>
2025-10-31 11:13:19 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ from io import BytesIO
 # --------------------
 # Database connection
 # --------------------
-DB_URL = "postgresql://neondb_owner:npg_axNXHM4suSA6@ep-crimson-mountain-a1thvyih-pooler.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
+DB_URL = st.secrets["DB_URL"]
 engine = create_engine(DB_URL)
 
 # --------------------
@@ -198,3 +198,4 @@ elif menu=="Histori Transaksi":
     else:
         st.dataframe(df_tx)
         st.download_button("Download CSV", data=df_to_csv_bytes(df_tx), file_name="transactions.csv")
+
