75394e2c44bb1b162716e64ada9a65c3689d15a8
FirdainiAzmi <firdaini.23071@mhs.unesa.ac.id>
2025-11-08 11:07:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -478,8 +478,8 @@ def page_daftar_voucher():
 
     # df_display = df.copy()
     df_display["initial_value"] = df_display["initial_value"].apply(lambda x: f"Rp {int(x):,}")
-        df_display["balance"] = df_display["balance"].apply(lambda x: f"Rp {int(x):,}")
-        df_display["created_at"] = pd.to_datetime(df_display["created_at"]).dt.strftime("%Y-%m-%d")
+    df_display["balance"] = df_display["balance"].apply(lambda x: f"Rp {int(x):,}")
+    df_display["created_at"] = pd.to_datetime(df_display["created_at"]).dt.strftime("%Y-%m-%d")
         
         # Cek aman untuk tanggal_penjualan
         if "tanggal_penjualan" in df_display.columns:
@@ -862,6 +862,7 @@ elif page == "Laporan Warung":
         page_laporan_global()
 else:
     st.info("Halaman tidak ditemukan.")
+
 
 
 
