9c02848fc080af4e3a0b9a23af23205fded5521b
FirdainiAzmi <firdaini.23071@mhs.unesa.ac.id>
2025-11-22 15:54:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2380,8 +2380,6 @@ if st.session_state.seller_logged_in and not st.session_state.admin_logged_in:
 if st.session_state.kasir_logged_in and not st.session_state.admin_logged_in:
     page_kasir()
     st.stop()
-        
-update_menu_item
 
 
 
@@ -2399,23 +2397,3 @@ update_menu_item
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-with tab1
