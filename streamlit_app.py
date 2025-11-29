f097254d60dd28fc21a499f9a59883a8fdea9c22
AriqHibatullah <ariqhibatullah03@gmail.com>
2025-11-29 15:21:57 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2284,7 +2284,7 @@ def page_seller_activation():
                         code,
                         initial_value,
                         status,
-                        tanggal_aktivasi,
+                        tanggal_aktivasi
                     FROM vouchers
                     WHERE code = :code
                     LIMIT 1
@@ -2802,6 +2802,7 @@ if st.session_state.seller_logged_in and not st.session_state.admin_logged_in:
 if st.session_state.kasir_logged_in and not st.session_state.admin_logged_in:
     page_kasir()
     st.stop()
+
 
 
 
