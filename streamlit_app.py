f1bd6d56003f75f8fb59c3437384ea9d02c2dd76
FirdainiAzmi <firdaini.23071@mhs.unesa.ac.id>
2025-12-01 10:06:44 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2857,4 +2857,3 @@ if st.session_state.seller_logged_in and not st.session_state.admin_logged_in:
 if st.session_state.kasir_logged_in and not st.session_state.admin_logged_in:
     page_kasir()
     st.stop()
-
